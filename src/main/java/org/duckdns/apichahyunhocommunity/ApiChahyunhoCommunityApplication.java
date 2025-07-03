1e5066c593ca72fbdfb186097d46a84cef0b0335
chahyeonho <chamym@naver.com>
2025-07-03 20:09:27 +0900

    initial commit

---
+++ src/main/java/org/duckdns/apichahyunhocommunity/ApiChahyunhoCommunityApplication.java
@@ -0,0 +1,13 @@
+package org.duckdns.apichahyunhocommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiChahyunhoCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiChahyunhoCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/duckdns/apichahyunhocommunity/ApiChahyunhoCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.duckdns.apichahyunhocommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiChahyunhoCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
